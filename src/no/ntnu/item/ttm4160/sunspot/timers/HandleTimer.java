f2c667b11c0c566c158775663c9f6bd6b39f92b8
nima732 <nima7321@gmail.com>
2012-11-07 14:01:00 +0100

    HandlerTimer

--- src/no/ntnu/item/ttm4160/sunspot/timers/HandleTimer.java
+++ src/no/ntnu/item/ttm4160/sunspot/timers/HandleTimer.java
@@ -12,27 +12,43 @@ public class HandleTimer {
 	
 	private static Vector activeTimers = new Vector();
 	
-	public static void addTimer(SpotTimer t)
+	public static void addTimer(SpotTimer spotTimer)
 	{
-		boolean found = false;
-		int index = 0;
-		SpotTimer buffer;
-		while(activeTimers.size()>index && !found)
-		{
-			 buffer = (SpotTimer) activeTimers.elementAt(index);
-			 if(buffer.getTimeout()>t.getTimeout())
-			 {
-				 //activeTimers.insertElementAt(obj, index)
-//				 my test!!!
-			 }
+		long timeOut = spotTimer.getTimeout();
+		if (activeTimers.size() < 1){
+			activeTimers.addElement(spotTimer);
+		}else{
+			for (int i = 0; i <= activeTimers.size();i++){
+				if(((SpotTimer)activeTimers.elementAt(i)).getTimeout() >= timeOut){
+					activeTimers.insertElementAt(spotTimer, i);
+					break;
+				}else if (activeTimers.size() == (i+1)){
+					activeTimers.addElement(spotTimer);
+				}
+					
+			}
 		}
 	}
 	
 	
-	//TODO boolean?
-	public static void removeTimer(SpotTimer t)
+	public static void removeTimer(SpotTimer spotTimer)
 	{	
-		activeTimers.removeElement(t);
+		activeTimers.removeElement(spotTimer);
+	}
+	
+	/*
+	 * This method return null if the Vector is empty or the is no any Timer which is expired. Otherwise it will return SpotTimer. 
+	 */
+	public static SpotTimer isTimeExpiured(){
+		long currentTime = System.currentTimeMillis();
+		
+		if (activeTimers.size() == 0){
+			return null;
+		}
+		if (((SpotTimer)activeTimers.elementAt(0)).getTimeout() <= currentTime){
+			return (SpotTimer)activeTimers.elementAt(0);
+		}
+		return null;
 	}
 
 }
